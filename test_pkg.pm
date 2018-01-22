bb8b9e5b21dbc246450a7b695b05b5e52ae24b20
ga@fyN <you@example.com>
2018-01-22 23:23:04 +0900

    for test of new package tagger

---
+++ test_pkg.pm
@@ -0,0 +1,23 @@
+package dayofweek {
+  our $sunday = "nitiyo";
+  our $monday = "getuyo";
+};
+
+package month;
+  our $jan = 1;
+  our $dec = 12;
+
+{
+  package  tvchannel;
+    sub new(){
+      my $name = "tvchannel";
+
+      my $self = {
+        tbs => 4,
+        asashi => 5,
+        ntv => 6,
+      };
+      return bless $self, $name;
+    }
+}
+1;
