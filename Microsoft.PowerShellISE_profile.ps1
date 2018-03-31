828bc423628c03e930d8a4eae9225427adc52220
ga@fyN <you@example.com>
2018-03-31 16:19:28 +0900

    ps1 profile

---
+++ Microsoft.PowerShellISE_profile.ps1
@@ -0,0 +1,29 @@
+#Requires -Version 3.0 
+#C:\Users\__YOUR_ACCOUNT__\Documents\WindowsPowerShell\Microsoft.PowerShellISE_profile.ps1
+
+function foo {cd c:\foo}
+function multiline { foo;  & 'C:\Program Files (x86)\AutoHotkey\AutoHotkey.exe' bar.ahk }
+function vi($files){
+c:\cast\app\gvim64\gvim.exe $files
+} 
+
+Function str2arr([string] $s, [string] $delim = "\s+", [int] $max = 0)
+{
+    $a = @()
+    $s -split $delim, $max | % {
+        $i = $_ -replace "^\s+", ""
+        $i = $i -replace "\s+$", ""
+        if($i.length){
+            $a += $i
+        }
+    }
+
+    return $a
+}
+
+function invokeCom([string] $member, [array] $args){
+	$c = New-Object -Comobject Fubar.COM
+	$ret = $c.getType().InvokeMember($member,
+		[Reflection.BindingFlags]::InvokeMethod, $null, $c, $args)
+	return $ret
+}
